48ef5f5c5c5bf83a3f77d0638dd59e2f154d78d8
Alexandr Gul <alexandr.gul@gmail.com>
2020-12-17 03:26:59 +0200

    Change caption on authorization page

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -49,7 +49,7 @@ export const AuthPage = () => {
     return (
         <div className={'row'}>
             <div className="col s12 m10">
-                <h1>Shot link creating</h1>
+                <h1>Gallery scheduler</h1>
                 <div className="card blue darken-1">
                     <div className="card-content white-text">
                         <span className="card-title">Authorization</span>
