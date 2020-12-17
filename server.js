fab3ee7bda045cbd7f46aaa29112c526f3a33b99
Alexandr Gul <alexandr.gul@gmail.com>
2020-12-17 02:45:31 +0200

    Fixed routes issue

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/gallery', require('./routes/gallery.routes'));
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
